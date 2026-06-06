f4fab508086c2713eda8388814cbcbefd0f4811d
Rafael Morales <Rafaelwmorales@gmail.com>
2026-06-06 15:45:40 -0300

    feat:arquivo JS junto com o link para a mudança de temas na página

---
+++ script.js
@@ -0,0 +1,14 @@
+function mudarTema(tema){
+    if(tema == "verde"){
+        document.body.style.background =
+        "linear-gradient(135deg,#0d3b0d,#1e7d32,#4caf50)";
+}
+    if(tema == "escuro"){
+        document.body.style.background =
+        "linear-gradient(135deg,#000,#111,#222)";
+}
+    if(tema == "padrao"){
+        document.body.style.background =
+        "linear-gradient(135deg,#12002f,#250062,#860063)"
+    }   
+}
