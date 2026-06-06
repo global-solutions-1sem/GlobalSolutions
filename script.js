a016f7bb4149636553cc3d9af7620bdc518e6bfb
Rafael Morales <Rafaelwmorales@gmail.com>
2026-06-06 16:12:26 -0300

    feat: implementação da função de pedir analise e do mini quiz na página sobre a página

--- script.js
+++ script.js
@@ -41,3 +41,74 @@ criarSlideshow("satSlide", [
     "./icons/satelite-agricultura.png"
 ], 2500); // função pra fazer 2 slideshows para a meia duzia de imagens que estão no projeto
 // ele percorre as duas listas de pngs sem parar por causa do setInterval
+
+document.getElementById("formulario").addEventListener("submit", function(e){ // procura o elemento no html
+
+    e.preventDefault(); // impede a pagina de dar F5 toda vez que o usuário pede uma análise
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+
+    if(nome === "" || email === ""){ // se for nulo ele vai pedir pro usuário digitar novamente
+        document.getElementById("mensagem").textContent =
+        "Preencha todos os campos!";
+        return;
+    }
+
+    document.getElementById("mensagem").textContent =
+    "Solicitação enviada com sucesso!";
+});
+
+const perguntas = [
+{
+    pergunta: "Qual tecnologia utilizamos para monitorar as plantações?",
+    opcoes: ["IoT", "Satélites", "Bluetooth"],
+    correta: 1
+},
+{
+    pergunta: "Qual índice é usado para analisar a vegetação?",
+    opcoes: ["NDVI", "HTML", "GPS"],
+    correta: 0
+},
+{
+    pergunta: "Qual tecnologia ajuda a interpretar os dados coletados?",
+    opcoes: ["IA", "Paint", "Excel"],
+    correta: 0
+}
+];
+
+let indice = 0;
+let pontos = 0;
+
+function carregarPergunta(){
+
+    document.getElementById("pergunta").textContent =
+        perguntas[indice].pergunta;
+
+    document.getElementById("btn0").textContent =
+        perguntas[indice].opcoes[0];
+
+    document.getElementById("btn1").textContent =
+        perguntas[indice].opcoes[1];
+
+    document.getElementById("btn2").textContent =
+        perguntas[indice].opcoes[2];
+}
+
+function responder(opcao){
+
+    if(opcao === perguntas[indice].correta){
+        pontos++;
+    }
+
+    indice++;
+
+    if(indice < perguntas.length){
+        carregarPergunta();
+    }else{
+        document.getElementById("pontuacao").textContent =
+        `Você acertou ${pontos} de ${perguntas.length} questões.`;
+    }
+}
+
+carregarPergunta();
